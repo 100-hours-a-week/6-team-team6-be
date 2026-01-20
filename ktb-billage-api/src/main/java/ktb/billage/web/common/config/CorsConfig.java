2e034f6ef2bacaf6b28d3942b3ee1bc815a632f2
hoooonshub <yhn515@naver.com>
2026-01-21 07:03:38 +0900

    feat: cors 설정 파일 추가

---
+++ ktb-billage-api/src/main/java/ktb/billage/web/common/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package ktb.billage.web.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    private static final List<String> ALLOWED_METHODS = List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    private static final List<String> ALLOWED_HEADERS = List.of("Authorization", "Content-Type", "Accept", "X-XSRF-TOKEN");
+    private static final String allowedPattern = "/**";
+
+    @Value("${cors.allowed.origin}")
+    private String[] allowedOrigins;
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(allowedOrigins));
+        config.setAllowedMethods(ALLOWED_METHODS);
+        config.setAllowedHeaders(ALLOWED_HEADERS);
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration(allowedPattern, config);
+        return source;
+    }
+}
