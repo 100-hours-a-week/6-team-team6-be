a679ecdc46f6a0a63bfad445f8a505f42723e31c
hoooonshub <yhn515@naver.com>
2026-01-21 08:08:55 +0900

    feat: 공통 예외 포맷 및 전역 예외 핸들러 추가

---
+++ ktb-billage-global/src/main/java/ktb/billage/exception/BaseException.java
@@ -0,0 +1,18 @@
+package ktb.billage.exception;
+
+public class BaseException extends RuntimeException {
+    private final ExceptionCode exceptionCode;
+
+    public BaseException(ExceptionCode exceptionCode) {
+        super(exceptionCode.toString());
+        this.exceptionCode = exceptionCode;
+    }
+
+    public int getStatusCode() {
+        return this.exceptionCode.getStatusCode();
+    }
+
+    public String getCode() {
+        return this.exceptionCode.getCode();
+    }
+}
