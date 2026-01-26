1a60abfee8143bc7503c5ab7c9d0a6671530eef1
hoooonshub <yhn515@naver.com>
2026-01-26 14:26:24 +0900

    feat: 멤버십 궈한 검증 및 식별자 조회 추가

---
+++ ktb-billage-group/src/main/java/ktb/billage/domain/membership/service/MembershipService.java
@@ -0,0 +1,21 @@
+package ktb.billage.domain.membership.service;
+
+import ktb.billage.common.exception.GroupException;
+import ktb.billage.domain.membership.Membership;
+import ktb.billage.domain.membership.MembershipRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static ktb.billage.common.exception.ExceptionCode.NOT_GROUP_MEMBER;
+
+@Service
+@RequiredArgsConstructor
+public class MembershipService {
+    private final MembershipRepository membershipRepository;
+
+    public Long findMembership(Long groupId, Long userId) {
+        return membershipRepository.findByGroupIdAndUserId(groupId, userId)
+                .map(Membership::getId)
+                .orElseThrow(() ->  new GroupException(NOT_GROUP_MEMBER));
+    }
+}
