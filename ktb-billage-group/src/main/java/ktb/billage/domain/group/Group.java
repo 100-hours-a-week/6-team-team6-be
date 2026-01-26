a3ca8b453a04b116ea562c04a438010a7db4b8d2
hoooonshub <yhn515@naver.com>
2026-01-26 14:25:55 +0900

    feat: 그룹 유효성 검증 추가

---
+++ ktb-billage-group/src/main/java/ktb/billage/domain/group/Group.java
@@ -0,0 +1,20 @@
+package ktb.billage.domain.group;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+}
