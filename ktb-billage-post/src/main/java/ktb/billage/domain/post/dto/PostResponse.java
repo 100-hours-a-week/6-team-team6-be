2294328e070174b5ac2ea31666db3517d7bdcff0
hoooonshub <yhn515@naver.com>
2026-01-26 20:44:32 +0900

    feat: 커서 기반 게시글 조회 기능 추가

--- ktb-billage-post/src/main/java/ktb/billage/domain/post/dto/PostResponse.java
+++ ktb-billage-post/src/main/java/ktb/billage/domain/post/dto/PostResponse.java
@@ -1,10 +1,32 @@
 package ktb.billage.domain.post.dto;
 
+import ktb.billage.domain.post.FeeUnit;
 import ktb.billage.domain.post.RentalStatus;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public class PostResponse {
 
     public record Id(Long postId) {}
 
     public record ChangedStatus(Long postId, RentalStatus rentalStatus) {}
+
+    public record Summaries(
+            List<Summary> summaries,
+            String nextCursor,
+            Boolean hasNextPage
+    ) {
+    }
+
+    public record Summary(
+            Long postId,
+            String postTitle,
+            Long postImageId,
+            String postFirstImageUrl,
+            BigDecimal rentalFee,
+            FeeUnit feeUnit,
+            RentalStatus rentalStatus
+    ) {
+    }
 }
