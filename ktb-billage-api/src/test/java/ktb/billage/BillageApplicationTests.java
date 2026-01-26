98ac16ce109e2a05a2430f8393dde81c617129be
hoooonshub <yhn515@naver.com>
2026-01-26 14:35:09 +0900

    test: 테스트 yml 파일 분리

--- ktb-billage-api/src/test/java/ktb/billage/BillageApplicationTests.java
+++ ktb-billage-api/src/test/java/ktb/billage/BillageApplicationTests.java
@@ -2,8 +2,10 @@ package ktb.billage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BillageApplicationTests {
 
     @Test
