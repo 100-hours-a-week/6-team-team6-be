92141405ec77aa95d35c8b1b620160f8e5e56cad
hoooonshub <yhn515@naver.com>
2026-01-27 22:39:37 +0900

    fix: 리턴 값 오해가 있을 수 있는 메서드 네이밍 수정

--- ktb-billage-group/src/main/java/ktb/billage/domain/membership/service/MembershipService.java
+++ ktb-billage-group/src/main/java/ktb/billage/domain/membership/service/MembershipService.java
@@ -7,14 +7,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import static ktb.billage.common.exception.ExceptionCode.NOT_GROUP_MEMBER;
-import static ktb.billage.common.exception.ExceptionCode.SELF_REQUEST_DENIED;
 
 @Service
 @RequiredArgsConstructor
 public class MembershipService {
     private final MembershipRepository membershipRepository;
 
-    public Long findMembership(Long groupId, Long userId) {
+    public Long findMembershipId(Long groupId, Long userId) {
         return membershipRepository.findByGroupIdAndUserId(groupId, userId)
                 .map(Membership::getId)
                 .orElseThrow(() ->  new GroupException(NOT_GROUP_MEMBER));
@@ -23,13 +22,6 @@ public class MembershipService {
     public void validateMembership(Long groupId, Long userId) {
         if (!membershipRepository.existsByGroupIdAndUserId(groupId, userId)) {
             throw new GroupException(NOT_GROUP_MEMBER);
-        }
-    }
-
-    public void validateNotSelf(Long userId, Long membershipId) {
-        Membership membership = findMembership(membershipId);
-        if (membership.isOwnedBy(userId)) {
-            throw new GroupException(SELF_REQUEST_DENIED);
         }
     }
 
