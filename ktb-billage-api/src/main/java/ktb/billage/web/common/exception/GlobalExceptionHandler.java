01a1df922253e12faaab63ee2d171eaa8dc48817
hoooonshub <yhn515@naver.com>
2026-01-28 20:48:01 +0900

    fix: 예외 처리 핸들러 @ControllerAdvice -> @RestControllerAdvice 변경

--- ktb-billage-api/src/main/java/ktb/billage/web/common/exception/GlobalExceptionHandler.java
+++ ktb-billage-api/src/main/java/ktb/billage/web/common/exception/GlobalExceptionHandler.java
@@ -8,13 +8,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
