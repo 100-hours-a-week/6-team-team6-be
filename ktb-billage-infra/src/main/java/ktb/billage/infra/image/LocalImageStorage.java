c8b6cb8d9203473af30da62f82a7aa32d140f002
hoooonshub <yhn515@naver.com>
2026-01-28 14:02:44 +0900

    feat: 로컬 및 테스트 용 이미지 저장소 추가

---
+++ ktb-billage-infra/src/main/java/ktb/billage/infra/image/LocalImageStorage.java
@@ -0,0 +1,31 @@
+package ktb.billage.infra.image;
+
+import ktb.billage.contract.image.ImageStorage;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Profile({"local", "test"})
+@Component
+public class LocalImageStorage implements ImageStorage {
+    private final Map<String, LocalImage> imageDatabase = new ConcurrentHashMap<>();
+
+    @Override
+    public String store(byte[] bytes, String contentType, long size) {
+        String id = UUID.randomUUID().toString();
+
+        imageDatabase.put(id, new LocalImage(bytes, contentType, size));
+        return id;
+    }
+
+    @Override
+    public void remove(String imageUrl) {
+
+    }
+
+    private record LocalImage(byte[] bytes, String contentType, long size) {
+    }
+}
