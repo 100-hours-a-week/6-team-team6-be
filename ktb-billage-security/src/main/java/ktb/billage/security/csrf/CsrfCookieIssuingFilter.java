6fc98cfe5705e44f587d2b14a0a14ad2bf517ca7
hoooonshub <yhn515@naver.com>
2026-02-01 17:17:20 +0900

    fix: csrf 토큰을 발급 순서를 필터 in bound시로 변경경

--- ktb-billage-security/src/main/java/ktb/billage/security/csrf/CsrfCookieIssuingFilter.java
+++ ktb-billage-security/src/main/java/ktb/billage/security/csrf/CsrfCookieIssuingFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.util.matcher.RequestMatcher;
@@ -12,6 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class CsrfCookieIssuingFilter extends OncePerRequestFilter {
     private final CsrfTokenRepository csrfTokenRepository;
@@ -19,12 +21,11 @@ public class CsrfCookieIssuingFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
-        filterChain.doFilter(request, response);
-
         if (csrfTokenIssueMatcher.matches(request)) {
             CsrfToken csrfToken = csrfTokenRepository.generateToken(request);
             csrfTokenRepository.saveToken(csrfToken, request, response);
         }
+
+        filterChain.doFilter(request, response);
     }
 }
