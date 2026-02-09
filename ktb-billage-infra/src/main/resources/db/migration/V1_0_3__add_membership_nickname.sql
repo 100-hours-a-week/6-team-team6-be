16583d5350b1a0e595a18ea188dd964afb309476
hoooonshub <yhn515@naver.com>
2026-02-09 19:25:43 +0900

    flyway: group에 baseentity 필드 추가

--- ktb-billage-infra/src/main/resources/db/migration/V1_0_3__add_membership_nickname.sql
+++ ktb-billage-infra/src/main/resources/db/migration/V1_0_3__add_membership_nickname.sql
@@ -3,3 +3,8 @@ ALTER TABLE membership
 
 ALTER TABLE users
     DROP COLUMN nickname;
+
+ALTER TABLE billage_group
+    ADD COLUMN created_at datetime(6) DEFAULT NULL,
+    ADD COLUMN updated_at datetime(6) DEFAULT NULL,
+    ADD COLUMN deleted_at datetime(6) DEFAULT NULL;
