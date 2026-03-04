da8ef94bef25251f1341dbdb5b84a8a611e444f7
hoooonshub <yhn515@naver.com>
2026-03-04 22:53:09 +0900

    docs: FCM 주석 추가

--- ktb-billage-websocket/src/main/java/ktb/billage/websocket/application/ChatPushEventListener.java
+++ ktb-billage-websocket/src/main/java/ktb/billage/websocket/application/ChatPushEventListener.java
@@ -3,11 +3,13 @@ package ktb.billage.websocket.application;
 import ktb.billage.websocket.application.event.ChatInboxSendEvent;
 import ktb.billage.websocket.application.port.ChatPushNotifier;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ChatPushEventListener {
@@ -16,6 +18,7 @@ public class ChatPushEventListener {
     @Async("chatInboxAsyncExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(ChatInboxSendEvent event) {
+        log.info("ChatPushEventListener Arrvied");
         chatPushNotifier.sendPush(event.receiveUserId(), event.ack());
     }
 }
